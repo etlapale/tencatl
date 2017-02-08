afbcd44744cc5acc9a10651c2171311325d7a4a0
Émilien Tlapale <emilien@tlapale.com>
2017-02-08 07:53:50 +0100

    Adding realistic test to lexer set.

--- tests/test-lexer.cc
+++ tests/test-lexer.cc
@@ -35,3 +35,44 @@ TEST(TestLexer, LexEmptyString)
   // Reading after the end of file should throw
   EXPECT_THROW(lexer.read_token(), std::runtime_error);
 }
+
+TEST(TestLexer, LexFib)
+{
+  using tencatl::Token;
+  
+  std::string input("# Recursively define the Fibonacci function\n"
+		    "def fib (x : int)\n"
+		    "  if x < 3\n"
+		    "    1\n"
+		    "  else\n"
+		    "    fib (x - 1) + fib (x - 2)\n");
+  tencatl::Lexer lexer;
+  lexer.set_source(std::make_unique<std::istringstream>(input));
+
+  std::vector<tencatl::Token> truth {
+    // def · fib · (
+    Token::Variable, Token::Variable, Token::Symbol,
+    // x · : · int
+    Token::Variable, Token::Symbol, Token::Variable,
+    // ) · → · if
+    Token::Symbol, Token::BlockBegin, Token::Variable,
+    // x · < · 3
+    Token::Variable, Token::Operator, Token::Int,
+    // → · 1 · ;
+    Token::BlockBegin, Token::Int, Token::EndOfExpression,
+    // ← · else · →
+    Token::BlockEnd, Token::Variable, Token::BlockBegin,
+    // fib · ( · x
+    Token::Variable, Token::Symbol, Token::Variable
+  };
+    
+  // Check the tokens agains ground truth
+  std::vector<tencatl::Token> tokens;
+  tencatl::Token token;
+  do {
+    token = lexer.read_token();
+    tokens.push_back(token);
+  } while (token != tencatl::Token::EndOfFile);
+  ASSERT_GE(tokens.size(), truth.size());
+  EXPECT_TRUE(equal(truth.cbegin(), truth.cend(), tokens.cbegin()));
+}
